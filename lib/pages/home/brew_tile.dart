b1bc31f351aa505d45a402b22f391c17750ae4f1
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-04-06 22:19:50 +0800

    Implement several assets

--- lib/pages/home/brew_tile.dart
+++ lib/pages/home/brew_tile.dart
@@ -15,7 +15,8 @@ class BrewTile extends StatelessWidget {
         child: ListTile(
           leading: CircleAvatar(
             radius: 25.0,
-            backgroundColor: Colors.brown[brew.strength]
+            backgroundColor: Colors.brown[brew.strength],
+            backgroundImage: AssetImage('assets/coffee_icon.png')
           ),
           title: Text(brew.name),
           subtitle: Text('Takes ${brew.sugars} sugar(s)'),
